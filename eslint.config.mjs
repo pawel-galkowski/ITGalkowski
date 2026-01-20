cf15ce293e860bc96821cf66b60166b6bde3cf36
Pawel Galkowski <xrocksdevilx@gmail.com>
2026-01-20 15:10:08 +0100

    update files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,7 +36,7 @@ const eslintConfig = defineConfig([
       "unused-imports/no-unused-vars": [
         "warn",
         { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
-      ]
+      ],
     },
   },
 
