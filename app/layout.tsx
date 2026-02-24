90e167944123ea965930e2fa655e9e5ff2503df2
Pawel Galkowski <xrocksdevilx@gmail.com>
2026-02-24 13:00:50 +0100

    update props

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { Providers } from "./providers";
 import "./globals.css";
 import { Languages } from "./i18n/types";
 import CleanupGrammarly from "./components/CleanupGrammarly";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -14,6 +16,8 @@ export default function RootLayout({
         <Providers>
           <CleanupGrammarly />
           {children}
+          <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
